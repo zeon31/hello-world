41d1f5bb814df755acbbe8cb078b3daac48773b4
zeon31 <rell.vineza14@yahoo.com>
2017-04-21 16:25:30 +0800

    added function

--- sample.sh
+++ sample.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+function hello () {
+  echo "I'm from a function"
+}
+
 echo "I'm the first line here"
 echo "I'm the second line and another one"
 echo "I'm the third line"
+hello
 
 exit 0
